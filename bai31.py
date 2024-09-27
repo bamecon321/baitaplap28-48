13d3be4e298792680cd6654681a08ce9fad90763
YxngHuu <bamecon321@gmail.com>
2024-09-27 20:25:16 +0700

    Update bai31.py

--- bai31.py
+++ bai31.py
@@ -9,7 +9,7 @@ Created on Sat Sep 21 13:49:07 2024
 a = int(input("Nhap vao so a: "))
 b = int(input("Nhap vao so b: "))
 c = int(input("Nhao vao so c: "))
-if a + b > c or b + c > a or a + c > a and (a > b > c > 0):
+if a + b > c or b + c > a or a + c > b and (a > b > c > 0):
     print("Day la 1 tam giac")
     if a == b or b == c or c == a:
       print("Day la tam giac can")

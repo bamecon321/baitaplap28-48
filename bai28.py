7faa89c4786780daa26cc7c1b48fab64d4b2aab9
YxngHuu <bamecon321@gmail.com>
2024-09-27 19:52:22 +0700

    Update bai28.py

--- bai28.py
+++ bai28.py
@@ -5,11 +5,12 @@ Created on Sat Sep 21 12:35:16 2024
 @author: ACER
 """
 
-while True:
-    a = int(input("Nhap so nguyen duong N: "))
-    if a > 0:
-        print("Hop le", a)
-        break
-    else:
-        print("Khong hop le")
-        
+N = int(input("Nhap so nguyen duong N: "))
+while N <= 0:
+    print("Số N phải là số nguyên dương.")
+    N = int(input("Nhập lại : "))
+
+
+for i in range(1, N+1):
+    if N % i == 0:
+        print(i, end=" \t")

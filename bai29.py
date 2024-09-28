c14d97427b4dce89c698287a0357526a3dfa4f96
YxngHuu <bamecon321@gmail.com>
2024-09-28 10:36:36 +0700

    Update bai29.py

--- bai29.py
+++ bai29.py
@@ -8,6 +8,8 @@ Created on Sat Sep 21 12:55:33 2024
 a = int(input("Nhap so N nguyen duong: "))
 tong = 0
 while a > 0:
+    a = int(input("Nhap lai N: "))
+for i in range(1,n+1)
     tong = tong + a % 10
     a = a // 10
 print("Tong cac chu so la: ", tong)
